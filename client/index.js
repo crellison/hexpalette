ad6f56630bb2e0b0a895fe50f14eab11eda82fd9
crellison <cole.ellison@gmail.com>
2016-07-05 21:58:36 -0400

    fixed smooth color (mostly) and added text coloring style

--- client/index.js
+++ client/index.js
@@ -4,13 +4,44 @@ var _=require('underscore');
 
 // HELPER FUNCTIONS
 function parseTime() {
-	// return 
+	// too simple
 	return '#'+(Date.now().toString(16).toUpperCase().slice(-7,-1))
 }
-function lightEnuf(color) {
-	color = color.split('')
-	if (color[1]==='f'||color[3]==='f'||color[5]==='f') return true
-	return false
+
+function smoothParse() {
+	var now = Date.now()
+	var H = Math.floor((now/10)%1000)/1000
+	var L = Math.floor((now/1000)%100)/100
+	var S = 1
+	// console.log('H: ',H)
+	// console.log('L: ',L)
+	var R, G, B, v2
+
+	if (L < .5) v2 = L*(1+S)
+	else 				v2 = (L+S)-(S*L)
+
+	var v1 = 2*L-v2
+
+	var R = Math.floor(255*H2R(v1, v2, H+(1/3)))
+	var G = Math.floor(255*H2R(v1, v2, H))
+	var B = Math.floor(255*H2R(v1, v2, H-(1/3)))
+
+	return [R,G,B]
+}
+
+function H2R(v1, v2, vH) {
+	if (vH<0) vH+=1
+	if (vH>1) vH-=1
+	if ((6*vH)<1) return (v1+(v2-v1)*6*vH)
+	if ((2*vH)<1) return (v2)
+	if ((3*vH)<2) return (v1+(v2-v1)*6*((2/3)-vH))
+	return v1
+}
+function toRGB(array) {
+	return 'rgb('+array[0]+','+array[1]+','+array[2]+')'
+}
+function lightEnuf(array) {
+	return 360<_.reduce(array, function(memo, num) {return (num + memo)}, 0)
 }
 
 // INITIAL PAGE
@@ -36,12 +67,12 @@ var ColorTimePage = React.createClass({
     };
   },
 	click: function() {
-		this.setState({'chroma':parseTime()})
+		this.setState({'chroma':smoothParse()})
 		// console.log('current: ',this.state.chroma)
 	},
 	componentDidMount: function() {
 		console.log('called')
-		setInterval(this.click,25)
+		setInterval(this.click,10)
 	},
 	render: function() {
 		if (this.props.page ==='time') {
@@ -49,8 +80,8 @@ var ColorTimePage = React.createClass({
 			if (lightEnuf(this.state.chroma)) 
 				h1c = '#2e2e2e'
 			return <div id="canvas" onClick={this.click} 
-				style={{'backgroundColor':this.state.chroma}}>
-				<h1 id="color" style={{'color':h1c}}>{this.state.chroma}</h1>
+				style={{'backgroundColor':toRGB(this.state.chroma)}}>
+				<h1 id="color" style={{'color':h1c}}>{toRGB(this.state.chroma)}</h1>
 			</div>;
 		}
 	}
